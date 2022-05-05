8e7acd623eb6a335c53bae1bfe3b30e87d6772c9
liluhao <llh@lu.hao>
2022-05-05 14:16:12 +0800

    2022.5.5  再次提交gitignore文件

--- handler/handler.go
+++ handler/handler.go
@@ -16,7 +16,7 @@ type Response struct {
 
 //供所有的服务模块返回时调用，所以直接放在handler目录下
 /*在返回结构体中，固定有Code和Message参数，这两个参数是通过函数DecodeErr()解析error类型的变量而来，并填充在Response结构体中。
-Data域为interface{}类型，可以根据业务自己的需求来返▣，可以是map、int、string、struct、.array等Go语言变量类型。
+Data域为interface{}类型,可以根据业务自己的需求来返▣，可以是map、int、string、struct、.array等Go语言变量类型。
 */
 func SendResponse(c *gin.Context, err error, data interface{}) {
 	code, message := errno.DecodeErr(err)
